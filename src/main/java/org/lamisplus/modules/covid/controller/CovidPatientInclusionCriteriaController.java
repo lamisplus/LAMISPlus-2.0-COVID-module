b059323ece51e3ba1b7a4769c74e356a2cbb24ff
kenkirui <kenkirui@gmail.com>
2022-04-05 17:39:28 +0300

    inclusioncriteria endpoint

---
+++ src/main/java/org/lamisplus/modules/covid/controller/CovidPatientInclusionCriteriaController.java
@@ -0,0 +1,37 @@
+package org.lamisplus.modules.covid.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.lamisplus.modules.covid.domain.entity.InclusionCriteria;
+import org.lamisplus.modules.covid.domain.entity.PatientStatus;
+import org.lamisplus.modules.covid.service.InclusionCriteriaService;
+import org.lamisplus.modules.covid.service.PatientStatusService;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/covid/inclusioncriteria")
+public class CovidPatientInclusionCriteriaController {
+    private final InclusionCriteriaService service;
+
+    @PostMapping("")
+    public InclusionCriteria Add(@RequestBody InclusionCriteria criteria){
+        return service.Save(criteria);
+    }
+
+    @PutMapping("/{id}")
+    public InclusionCriteria Update(@PathVariable Long id, @RequestBody InclusionCriteria criteria){
+        return service.Update(criteria);
+    }
+
+    @GetMapping("")
+    public InclusionCriteria load(@PathVariable int id){
+        return service.load(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public String Delete(@PathVariable Integer id){
+        return service.Delete(id);
+    }
+}
--- src/main/java/org/lamisplus/modules/covid/repository/InclusionCriteriaRepository.java
+++ src/main/java/org/lamisplus/modules/covid/repository/InclusionCriteriaRepository.java
@@ -2,12 +2,10 @@ package org.lamisplus.modules.covid.repository;
 
 import lombok.RequiredArgsConstructor;
 import org.lamisplus.modules.covid.domain.entity.InclusionCriteria;
-import org.lamisplus.modules.covid.domain.entity.Patient;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -27,7 +25,7 @@ public class InclusionCriteriaRepository {
         return findByUUID(inclusionCriteria.getUuid()).orElse(null);
     }
 
-    public Optional<InclusionCriteria> update(InclusionCriteria inclusionCriteria) {
+    public InclusionCriteria update(InclusionCriteria inclusionCriteria) {
         jdbcTemplate.update("UPDATE covid_patient set uuid=?, covid_signs=?, covid_test=?, test_date=? " +
                         "where id=? ",
                 inclusionCriteria.getCovid_signs(),
@@ -35,12 +33,12 @@ public class InclusionCriteriaRepository {
                 inclusionCriteria.getTest_date(),
                 inclusionCriteria.getId()
         );
-        return findByUUID(inclusionCriteria.getUuid());
+        return findByUUID(inclusionCriteria.getUuid()).orElse(null);
     }
 
-    public List<InclusionCriteria> findAll() {
-        return jdbcTemplate.query("SELECT * FROM covid_inclusioncriteria",
-                new BeanPropertyRowMapper<InclusionCriteria>(InclusionCriteria.class));
+    public InclusionCriteria findByPatientId(int patient_id) {
+        return jdbcTemplate.query("SELECT * FROM covid_inclusioncriteria where patient_id = ?",
+                new BeanPropertyRowMapper<InclusionCriteria>(InclusionCriteria.class), patient_id).stream().findFirst().orElse(null);
     }
 
     public String delete(int id){
