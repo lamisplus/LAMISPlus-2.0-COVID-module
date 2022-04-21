e772a6c3ff8b66740a13e477a42cb63edba9fe81
Mathew Adegbite <mathewadegbite@gmail.com>
2022-04-21 16:31:32 +0100

    Updated code on patient Dashboard
    Updated code on patient Dashboard

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 
-export const  token = (new URLSearchParams(window.location.search)).get("jwt")
-export const url = '/api/'
-//export const url =  'http://localhost:8282/api/';
-//export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjQ5Nzg2OTQwfQ.-STlJMa3CmdoFurnCbgdcSwt7E7fW_AH-nTvn-uD8t_zMRHUVXChCFpr5kRqP0LbHlvjDuRijNgwnpYHzMvtWQ';
+//export const  token = (new URLSearchParams(window.location.search)).get("jwt")
+//export const url = '/api/'
+export const url =  'http://localhost:8282/api/';
+export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjUwNDg4NjI5fQ.IZzRBTUicDAj5KjMB3Ptvh6h8DlcAZ5-oCUK33UTcBkaYA8-wOLZB6exCrT11F5quyhHbCknETTnVrCj87vLxg';
